f257e3ce9a0dd7a591717d581875cf993e678caa
Yang Zhifei <xividyzf@gmail.com>
2014-08-03 11:13:24 +0800

    ... Fix a silly error...

--- Downloader/download.sh
+++ Downloader/download.sh
@@ -5,13 +5,13 @@ for month in {01..12}; do
 	echo "Downloading archive of 2013.${month}..."
 	case $month in
 		01 | 03 | 05 | 07 | 08 | 10 | 12)
-			echo http://www.baidu.com/2013-$month-{01..31}-{0..23}.json.gz
+			wget http://data.githubarchive.org/2013-$month-{01..31}-{0..23}.json.gz
 			;;
 		02)
-			echo http://www.baidu.com/2013-02-{01..28}-{0..23}.json.gz
+			wget http://data.githubarchive.org/2013-02-{01..28}-{0..23}.json.gz
 			;;
 		*)
-			echo http://www.baidu.com/2013-$month-{01..30}-{0..23}.json.gz
+			wget http://data.githubarchive.org/2013-$month-{01..30}-{0..23}.json.gz
 			;;
 	esac
 	echo "============="
