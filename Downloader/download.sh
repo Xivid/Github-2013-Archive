eecf20e62ecce5317a9249850324261aea88d24a
Yang Zhifei <xividyzf@gmail.com>
2014-08-03 11:10:29 +0800

    Shell test ok

--- Downloader/download.sh
+++ Downloader/download.sh
@@ -1,1 +1,19 @@
-wget http://data.githubarchive.org/2012-04-11-15.json.gz
+#!/bin/zsh
+#This shell can be run correctly in any shell interpreting {01..02} as 01,02 rather than 1,2.
+
+for month in {01..12}; do
+	echo "Downloading archive of 2013.${month}..."
+	case $month in
+		01 | 03 | 05 | 07 | 08 | 10 | 12)
+			echo http://www.baidu.com/2013-$month-{01..31}-{0..23}.json.gz
+			;;
+		02)
+			echo http://www.baidu.com/2013-02-{01..28}-{0..23}.json.gz
+			;;
+		*)
+			echo http://www.baidu.com/2013-$month-{01..30}-{0..23}.json.gz
+			;;
+	esac
+	echo "============="
+done
+echo "Congratulations! Download Succeeded. Bye!"
